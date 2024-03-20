e8253aa35dbe5ddbdcfc4e9c114606c26bf120b8
HONG SE HYUN <sese0733@naver.com>
2024-03-20 21:43:50 +0900

    design : 승패 결과에 따라 테두리 색상 바꾸기

--- src/component/Box.js
+++ src/component/Box.js
@@ -1,10 +1,23 @@
 import React from 'react'
 
 const Box = (props) => {
+  const borderColor = props.result === 'win' ? 'green' : props.result === 'lose' ? 'red' : 'black';
+
+  const boxStyle = {
+    border: `10px solid ${borderColor}`,
+    width: '500px',
+    height: '600px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: '20px',
+  };
+
   return (
-    <div className='box'>
+    <div style={boxStyle}>
         <h1>{props.title}</h1>
-        <img className='item-img' src={props.item && props.item.img}/>
+        <img className='item-img' src={props.item && props.item.img} alt=""/>
         <h2>{props.result}</h2>
     </div>
   )
